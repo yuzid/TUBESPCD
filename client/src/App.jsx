f2efca1465aecc2fc149ff95ad35f1c5e10f2c4c
yuzid <yazid.fauzan9999@gmail.com>
2025-06-14 10:38:58 +0700

    add main menu screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Game from './pages/game';
+import MainMenu from "./pages/menu";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Game/>}/>
-        <Route path="/menu" element={<></>}/>
+        <Route path="/" element={<MainMenu/>}/>
+        <Route path="/game" element={<Game/>}/>
       </Routes>
     </BrowserRouter>
   );
